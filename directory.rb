cd919d019570eeb43f8b6ef87c161abb68a15acb
Adam G <adamwizzy@hotmail.co.uk>
2020-10-21 15:51:41 +0100

    created interactive_menu method and moved other method calls within this, also removed center() method from all text

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 # creating methods to print information
 def input_students
-  puts "Please enter the names of the first student".center(200)
-  puts "To finish, don't enter a name and press return".center(200)
+  puts "Please enter the names of the first student"
+  puts "To finish, don't enter a name and press return"
   # create an empty array
   students = []
   # get the first name
@@ -9,32 +9,32 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # taking the cohort and handling
-    puts "Is this student in the february, may, august or november cohort?".center(200)
+    puts "Is this student in the february, may, august or november cohort?"
     legal_cohorts = ["february", "may", "august", "november"]
     cohort = gets.strip
     until legal_cohorts.include?(cohort)
-      puts "You entered an invalid cohort, please try again.".center(200)
+      puts "You entered an invalid cohort, please try again."
       cohort = gets.strip
     end
     # ask for hobby
-    puts "Please enter this student's favourite hobby".center(200)
+    puts "Please enter this student's favourite hobby"
     hobby = gets.strip
     hobby = "not provided" if hobby.empty?
     # ask for country of birth
-    puts "Please enter this student's country of birth".center(200)
+    puts "Please enter this student's country of birth"
     country_of_birth = gets.strip
     country_of_birth = "not provided" if country_of_birth.empty?
     # ask for height
-    puts "Please enter this student's height in cm".center(200)
+    puts "Please enter this student's height in cm"
     height = gets.strip
     height = "not provided" if height.empty?
     # add the student hash to the array
     students << {name: name.capitalize, cohort: cohort.to_sym, hobby: hobby.capitalize,
                  country_of_birth: country_of_birth.capitalize, height: height.capitalize}
-    puts "Now we have #{students.count} students".center(200) if students.count > 1
-    puts "Now we have #{students.count} student".center(200) if students.count == 1
+    puts "Now we have #{students.count} students" if students.count > 1
+    puts "Now we have #{students.count} student" if students.count == 1
     # get another name from the user
-    puts "Please enter the name of the next student, or press return to cancel".center(200)
+    puts "Please enter the name of the next student, or press return to cancel"
     name = gets.strip
   end
   # return array of students
@@ -42,25 +42,22 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(200)
-  puts "-------------".center(200)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(people)
   cohort_hash = {february: [], may: [], august: [], november: []}
   people.each do |person|
     cohort_hash[person[:cohort]].push(person)
-    # puts "#{person_number}. #{person[:name]} (#{person[:cohort]} cohort)".center(200)
-    # puts "   #{person[:name]} was born in #{person[:country_of_birth]}, is #{person[:height]} cm tall and enjoys #{person[:hobby]}.".center(200)
-    # person_number += 1
   end
   cohort_hash.each_pair do |cohort, people|
     if !people.empty?
       person_number = 1
-      puts "Students in #{cohort.to_s.capitalize} cohort:".center(200)
+      puts "Students in #{cohort.to_s.capitalize} cohort:"
       people.each do |person|
-        puts "#{person_number}. #{person[:name]}".center(200)
-        puts "#{person[:name]} was born in #{person[:country_of_birth]}, is #{person[:height]} cm tall and enjoys #{person[:hobby]}.".center(200)
+        puts "#{person_number}. #{person[:name]}"
+        puts "   #{person[:name]} was born in #{person[:country_of_birth]}, is #{person[:height]} cm tall and enjoys #{person[:hobby]}."
         person_number += 1
       end
       puts ""
@@ -69,12 +66,34 @@ def print(people)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(200) if names.count > 1
-  puts "Overall, we have #{names.count} great student".center(200) if names.count == 1
+  puts "Overall, we have #{names.count} great students" if names.count > 1
+  puts "Overall, we have #{names.count} great student" if names.count == 1
+end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" #we'll be adding more later
+    # 2. read and saveuser input
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students # if 1 is entered twice, the already entered students will be overwritten
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # terminates program
+    else
+      puts "Invalid selection, please try again"
+    end
+  end
 end
 
 # nothing happens until the methods are called
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
