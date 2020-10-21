b8c67e053993c6b299784de09a52a70720d8319c
Adam G <adamwizzy@hotmail.co.uk>
2020-10-21 11:53:14 +0100

    have added more information fields about the students which the user can input, and altered print method to include

--- directory.rb
+++ directory.rb
@@ -1,21 +1,43 @@
 # creating methods to print information
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the first student"
+  puts "To finish, don't enter a name and press return"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
-    #add the student hash to the array
-    students << {name: name.capitalize, cohort: :november}
+  while !name.empty? do #hobby, country of birth, height
+    # ask for hobby
+    puts "Please enter this student's favourite hobby"
+    hobby = gets.chomp
+    hobby = "not provided" if hobby.empty?
+    # ask for country of birth
+    puts "Please enter this student's country of birth"
+    country_of_birth = gets.chomp
+    country_of_birth = "not provided" if country_of_birth.empty?
+    # ask for height
+    puts "Please enter this student's height in cm"
+    height = gets.chomp
+    height = "not provided" if height.empty?
+    # add the student hash to the array
+    students << {name: name.capitalize, cohort: :november, hobby: hobby.capitalize,
+                 country_of_birth: country_of_birth.capitalize, height: height.capitalize}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter the name of the next student, or press return to cancel"
     name = gets.chomp
   end
-  #return array of students
+  # return array of students
   students
+end
+
+def print_specific_students
+  puts "Would you like to print all students or only those beginning with a specific letter?"
+  puts "Enter 'all' for all students or a letter for only students beginning with that letter"
+  letter = gets.chomp.upcase
+  letter = "ALL" if letter.empty?
+  letter
 end
 
 def print_header
@@ -23,21 +45,27 @@ def print_header
   puts "-------------"
 end
 
-def print(people)
-  # create a copy of the array which we can mutate without affecting student number count in print_footer method
-  copy_of_people = people.map(&:clone)
-  person_number = 1
-  # use of the while loop, deleting the 0th index in the copied array each time 
-  while !copy_of_people.empty?
-    if copy_of_people[0][:name].length < 12 # only those people with names under 12 chars. get printed
-      puts "#{person_number}. #{copy_of_people[0][:name]} (#{copy_of_people[0][:cohort]} cohort)"
-      person_number += 1
-      copy_of_people.delete_at(0)
-    else # people with names over 12 chars. still need to be removed from the array to exit the loop
-      copy_of_people.delete_at(0)
+def print(people, letter)
+  if letter == "ALL"
+    person_number = 1
+    people.each do |person|
+      if person[:name].length < 12
+        puts "#{person_number}. #{person[:name]} (#{person[:cohort]} cohort)"
+        puts "   #{person[:name]} was born in #{person[:country_of_birth]}, is #{person[:height]} cm tall and enjoys #{person[:hobby]}."
+        person_number += 1
+      end
+    end
+  else
+    person_number = 1
+    people.each do |person|
+      if person[:name][0] == letter && person[:name].length < 12
+        puts "#{person_number}. #{person[:name]} (#{person[:cohort]} cohort)"
+        puts "   #{person[:name]} was born in #{person[:country_of_birth]}, is #{person[:height]} cm tall and enjoys #{person[:hobby]}."
+        person_number += 1
+      end
     end
   end
-end # the use of the while loop to control the flow of this method is clunky and forced
+end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
@@ -45,6 +73,7 @@ end
 
 # nothing happens until the methods are called
 students = input_students
+letter_to_print = print_specific_students
 print_header
-print(students)
+print(students, letter_to_print)
 print_footer(students)
