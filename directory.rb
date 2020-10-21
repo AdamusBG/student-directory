642499b104e7c190c6c222b571aff3dffd0b5a9c
Adam G <adamwizzy@hotmail.co.uk>
2020-10-21 13:15:30 +0100

    replaced chomp with strip

--- directory.rb
+++ directory.rb
@@ -5,28 +5,28 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.strip
   # while the name is not empty, repeat this code
   while !name.empty? do
     # taking the cohort and handling
     puts "Is this student in the february, may, august or november cohort?".center(200)
     legal_cohorts = ["february", "may", "august", "november"]
-    cohort = gets.chomp
+    cohort = gets.strip
     until legal_cohorts.include?(cohort)
       puts "You entered an invalid cohort, please try again.".center(200)
-      cohort = gets.chomp
+      cohort = gets.strip
     end
     # ask for hobby
     puts "Please enter this student's favourite hobby".center(200)
-    hobby = gets.chomp
+    hobby = gets.strip
     hobby = "not provided" if hobby.empty?
     # ask for country of birth
     puts "Please enter this student's country of birth".center(200)
-    country_of_birth = gets.chomp
+    country_of_birth = gets.strip
     country_of_birth = "not provided" if country_of_birth.empty?
     # ask for height
     puts "Please enter this student's height in cm".center(200)
-    height = gets.chomp
+    height = gets.strip
     height = "not provided" if height.empty?
     # add the student hash to the array
     students << {name: name.capitalize, cohort: cohort.to_sym, hobby: hobby.capitalize,
@@ -35,7 +35,7 @@ def input_students
     puts "Now we have #{students.count} student".center(200) if students.count == 1
     # get another name from the user
     puts "Please enter the name of the next student, or press return to cancel".center(200)
-    name = gets.chomp
+    name = gets.strip
   end
   # return array of students
   students
