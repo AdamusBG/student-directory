f744724328a0ec65020cb0cbd967932b69da6075
Adam G <adamwizzy@hotmail.co.uk>
2020-10-22 13:40:34 +0100

    added the ability to pass file name as command line argument and load data from that file

--- directory.rb
+++ directory.rb
@@ -5,28 +5,28 @@ def input_students
   puts "Please enter the names of the first student"
   puts "To finish, don't enter a name and press return"
   # get the first name
-  name = gets.strip
+  name = STDIN.gets.strip
   # while the name is not empty, repeat this code
   while !name.empty? do
     # taking the cohort and handling
     puts "Is this student in the february, may, august or november cohort?"
     legal_cohorts = ["february", "may", "august", "november"]
-    cohort = gets.strip
+    cohort = STDIN.gets.strip
     until legal_cohorts.include?(cohort)
       puts "You entered an invalid cohort, please try again."
-      cohort = gets.strip
+      cohort = STDIN.gets.strip
     end
     # ask for hobby
     puts "Please enter this student's favourite hobby"
-    hobby = gets.strip
+    hobby = STDIN.gets.strip
     hobby = "not provided" if hobby.empty?
     # ask for country of birth
     puts "Please enter this student's country of birth"
-    country_of_birth = gets.strip
+    country_of_birth = STDIN.gets.strip
     country_of_birth = "not provided" if country_of_birth.empty?
     # ask for height
     puts "Please enter this student's height in cm"
-    height = gets.strip
+    height = STDIN.gets.strip
     height = "not provided" if height.empty?
     # add the student hash to the array
     @students << {name: name.capitalize, cohort: cohort.to_sym, hobby: hobby.capitalize,
@@ -35,7 +35,7 @@ def input_students
     puts "Now we have #{@students.count} student" if @students.count == 1
     # get another name from the user
     puts "Please enter the name of the next student, or press return to cancel"
-    name = gets.strip
+    name = STDIN.gets.strip
   end
 end
 
@@ -75,7 +75,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -124,8 +124,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort, country_of_birth, height, hobby = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym, hobby: hobby,
@@ -134,5 +134,18 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
 # nothing happens until the methods are called
+try_load_students
 interactive_menu
